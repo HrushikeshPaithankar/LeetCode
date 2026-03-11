d0f417a0afd91d8abc16d99c366ccdb634947e5a
HrushikeshPaithankar <hrushikesh282006@gmail.com>
2026-03-11 11:47:54 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (59.24%) - LeetSync

--- 1054-complement-of-base-10-integer/complement-of-base-10-integer.cpp
+++ 1054-complement-of-base-10-integer/complement-of-base-10-integer.cpp
@@ -1,26 +1,17 @@
 class Solution {
 public:
     int bitwiseComplement(int n) {
-        if (n==0)
+        if(n==0)
         {
             return 1;
         }
-        string s="";
-        while(n>0)
+        int mask=0;
+        int temp=n;
+        while(temp)
         {
-            s+=(n%2+'0');
-            n/=2;
+            mask=(mask<<1) | 1;
+            temp>>=1;
         }
-        reverse(s.begin(),s.end());
-        int ans=0;
-        int k=s.size()-1;
-        for(int i=0;i<s.size();i++)
-        {
-            if(s[i]=='0')
-            {
-                ans+=(1LL<<(k-i));
-            }
-        }
-        return ans;
+        return mask^n;
     }
 };
